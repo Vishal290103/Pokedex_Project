966d4d1996b4b20beec82603736e8ded6040d20e
Vishal290103 <vishalyadav29032003@gamil.com>
2024-12-24 23:38:51 +0530

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Pokedex from './components/Pokedex/Pokedex'
-
+import CustomRoutes from './routes/CustomRoutes'
+import {Link} from 'react-router-dom';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Pokedex/>
-    </>
+    <div className='outer-pokedex'>
+    <h1 id="pokedex-heading">
+      <Link to='/'>
+      Pokedex
+      </Link>
+      
+    </h1>
+
+    <CustomRoutes/>
+    </div>
   )
 }
 
